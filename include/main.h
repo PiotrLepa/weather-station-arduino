e9a1e0e26225a1cfa9fae5e915c830f4a9df0331
Piotr Łępa <piotr.lepa@altconnect.pl>
2020-06-03 20:40:28 +0200

    add lib & cleanup files

---
+++ include/main.h
@@ -0,0 +1,13 @@
+
+#ifndef MAIN_H
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include "config.h"
+
+void setup();
+void loop();
+
+#endif
