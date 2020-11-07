0714b09e528c42a5b139f39140e1fa152ef05ecc
Piotr Łępa <piotr.lepa@altconnect.pl>
2020-11-07 15:06:29 +0100

    implement rain detector

--- src/sensor/rain_gauge/rain_gauge_reader.cpp
+++ src/sensor/rain_gauge/rain_gauge_reader.cpp
@@ -6,9 +6,9 @@ void ICACHE_RAM_ATTR handleRainGaugePinInterrupt() { rainGaugeReaderInstance->co
 
 RainGaugeReader::RainGaugeReader(uint8_t _rainGaugeSensorPin)
     : rainGaugeSensorPin(_rainGaugeSensorPin),
+      rainDetectorLastTriggerTime(0),
       tips(0),
       bounceTime(0),
-      measurementCounter(0),
       errorMessage("No errors") {
   rainGaugeReaderInstance = this;
 }
@@ -16,7 +16,6 @@ RainGaugeReader::RainGaugeReader(uint8_t _rainGaugeSensorPin)
 void RainGaugeReader::begin() {}
 
 void RainGaugeReader::startReading() {
-  measurementCounter = 0;
   tips = 0;
   errorMessage = "No errors";
 
@@ -29,9 +28,24 @@ void ICACHE_RAM_ATTR RainGaugeReader::countTips() {
   if ((millis() - bounceTime) > ROTATION_DEBOUNCE_TIME) {
     tips++;
     bounceTime = millis();
+    handleRainDetector();
+
+    Serial.print("Tips: ");
+    Serial.println(tips);
   }
 }
 
 RainGaugeModel RainGaugeReader::getData() { return RainGaugeModel(tips * ML_FOR_TIP); }
 
 String RainGaugeReader::getErrorMessage() { return errorMessage; }
+
+void RainGaugeReader::setRainDetectedCallback(void (*callback)()) { rainDetectedCallback = callback; }
+
+void RainGaugeReader::handleRainDetector() {
+  long now = DateTime::now().getSecondsFromEpoch();
+  if (now == -1) return;
+  if (rainDetectorLastTriggerTime == 0 || now - rainDetectorLastTriggerTime >= RAIN_DETECTOR_DELAY_SECONDS) {
+    rainDetectorLastTriggerTime = now;
+    rainDetectedCallback();
+  }
+}
--- src/sensor/rain_gauge/rain_gauge_reader.h
+++ src/sensor/rain_gauge/rain_gauge_reader.h
@@ -3,6 +3,7 @@
 
 #define ROTATION_DEBOUNCE_TIME 15
 #define ML_FOR_TIP 2.5
+#define RAIN_DETECTOR_DELAY_SECONDS 3600
 
 #include <Arduino.h>
 #include <Ticker.h>
@@ -10,6 +11,7 @@
 #include <vector>
 
 #include "../../model/rain_gauge/rain_gauge_model.h"
+#include "../../utils/date_time/date_time.h"
 #include "../continuous_sensor_reader.h"
 
 class RainGaugeReader : public ContinuousSensorReader<RainGaugeModel> {
@@ -21,17 +23,21 @@ class RainGaugeReader : public ContinuousSensorReader<RainGaugeModel> {
   void stopReading() override;
   RainGaugeModel getData() override;
   String getErrorMessage() override;
+  void setRainDetectedCallback(void (*callback)());
 
   void ICACHE_RAM_ATTR countTips();
 
  private:
   uint8_t rainGaugeSensorPin;
+  long rainDetectorLastTriggerTime;
+  void (*rainDetectedCallback)();
 
   int tips;
   unsigned long bounceTime;
-  int measurementCounter;
 
   String errorMessage;
+
+  void handleRainDetector();
 };
 
 #endif
